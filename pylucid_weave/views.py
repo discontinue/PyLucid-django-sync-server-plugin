e6a522f159de49dc72c038dff42fbc2029781f54
JensDiemer <git@jensdiemer.de>
2010-07-06 15:01:52 +0200

    add url info

--- pylucid_weave/views.py
+++ pylucid_weave/views.py
@@ -5,13 +5,24 @@
 """
 
 from django.core.urlresolvers import reverse
-from django.http import Http404
+from django.contrib.auth.decorators import login_required
+
+from pylucid_project.apps.pylucid.decorators import render_to
 
 
+def absolute_uri(request, view_name, **kwargs):
+    url = reverse(view_name, kwargs=kwargs)
+    absolute_uri = request.build_absolute_uri(url)
+    return absolute_uri
+
+
+@login_required
+@render_to("pylucid_weave/url_info.html")
 def url_info(request):
-    if request.user.is_anonymous():
-        raise Http404
-
-#    url = reverse("weave-url_info")
-    url = request.build_absolute_uri()
-    return "Please use url '%s' in your weave client settings." % url
+    context = {
+        "title": "weave testproject url info",
+        "server_url": request.build_absolute_uri(),
+        "register_check_url": absolute_uri(request, "weave-register_check", username=request.user.username),
+        "info_url": absolute_uri(request, "weave-info", version="1.0", username=request.user.username),
+    }
+    return context
